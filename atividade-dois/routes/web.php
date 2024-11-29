f397e06556efc81fff9c28da8945aa71c3460470
Sócrates Souza de Luna <socratessouza18@gmail.com>
2024-11-29 08:31:53 -0300

    Construção do Controller, Rotas e Views para a Entidade Book

--- atividade-dois/routes/web.php
+++ atividade-dois/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\AuthorsController;
 use App\Http\Controllers\PublishersController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
@@ -16,3 +17,13 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('categories', CategoryController::class);
 Route::resource('author', AuthorsController::class);
 Route::resource('publishers', PublishersController::class);
+
+// Rotas para criação de livros
+Route::get('/books/create-id-number', [BookController::class, 'createWithId'])->name('books.create.id');
+Route::post('/books/create-id-number', [BookController::class, 'storeWithId'])->name('books.store.id');
+
+Route::get('/books/create-select', [BookController::class, 'createWithSelect'])->name('books.create.select');
+Route::post('/books/create-select', [BookController::class, 'storeWithSelect'])->name('books.store.select');
+
+// Rotas RESTful para index, show, edit, update, delete (tem que ficar depois das rotas /books/create-id-number e /books/create-select)
+Route::resource('books', BookController::class)->except(['create', 'store']);
